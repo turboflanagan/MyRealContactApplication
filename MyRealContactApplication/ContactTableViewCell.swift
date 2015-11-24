5c329f03dc32099cd3c42869a2daf1be5fbccff1
turboflanagan <turboflanagan@gmail.com>
2015-11-23 22:04:34 -0500

    Moved files from where they were to where they need to be.

---
+++ MyRealContactApplication/ContactTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ContactTableViewCell.swift
+//  3rd Contacts App
+//
+//  Created by MacBook Pro on 11/23/15.
+//  Copyright © 2015 Peter Flanagan. All rights reserved.
+//
+
+import UIKit
+
+class ContactTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
